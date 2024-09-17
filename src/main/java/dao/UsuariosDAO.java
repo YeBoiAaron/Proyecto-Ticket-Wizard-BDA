d8cea10b12e35b91f658ffcc6b11c6c2f0a36b5e
YeBoiAaron <60552917+YeBoiAaron@users.noreply.github.com>
2024-09-17 13:33:04 -0700

    Update UsuariosDAO.java

--- src/main/java/dao/UsuariosDAO.java
+++ src/main/java/dao/UsuariosDAO.java
@@ -28,27 +28,27 @@ public class UsuariosDAO implements IUsuariosDAO {
     @Override
     public boolean registrarUsuario(Usuario usuario) {
         try {
-            Connection c = conexion.crearConexion();
+            Connection connection = conexion.crearConexion();
             String insertar = "INSERT INTO usuarios (correo_electronico,contrasena,nombre_completo,fecha_nacimiento,edad,domicilio,saldo) VALUES (?,?,?,?,?,?,?)";
-            PreparedStatement i = c.prepareStatement(insertar, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement statement = connection.prepareStatement(insertar, Statement.RETURN_GENERATED_KEYS);
             
             // Establecer los valores de los atributos del usuario en el PreparedStatement
-            i.setString(1, usuario.getCorreo());
-            i.setString(2, usuario.getContrasena());
-            i.setString(3, usuario.getNombre_completo());
+            statement.setString(1, usuario.getCorreo());
+            statement.setString(2, usuario.getContrasena());
+            statement.setString(3, usuario.getNombre_completo());
 
             // Convertir la fecha de nacimiento de java.util.Date a java.sql.Date
             java.util.Date utilDate = usuario.getFecha_nacimiento();
             java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
 
             // Establecer la fecha en el PreparedStatement
-            i.setDate(4, sqlDate);
-            i.setInt(5, usuario.getEdad());
-            i.setString(6, usuario.getDomicilio());
-            i.setFloat(7, usuario.getSaldo());
+            statement.setDate(4, sqlDate);
+            statement.setInt(5, usuario.getEdad());
+            statement.setString(6, usuario.getDomicilio());
+            statement.setFloat(7, usuario.getSaldo());
             
             // Ejecutar la consulta para insertar el registro
-            i.executeUpdate();
+            statement.executeUpdate();
             
             return true;
         } catch(SQLException e) {
@@ -60,19 +60,19 @@ public class UsuariosDAO implements IUsuariosDAO {
     @Override
     public boolean actualizarUsuario(Usuario usuario) {
         try {
-            Connection c = conexion.crearConexion();
+            Connection connection = conexion.crearConexion();
             String actualizar = "UPDATE usuarios SET correo_electronico = ?,contrasena = ?,nombre_completo = ?,fecha_nacimiento = ?,edad = ?,domicilio = ?,saldo = ? WHERE id_usuario = ?";
-            PreparedStatement a = c.prepareStatement(actualizar, Statement.RETURN_GENERATED_KEYS);
-            a.setString(1, usuario.getCorreo());
-            a.setString(2, usuario.getContrasena());
-            a.setString(3, usuario.getNombre_completo());
-            a.setDate(4, (Date) usuario.getFecha_nacimiento());
-            a.setInt(5, usuario.getEdad());
-            a.setString(6, usuario.getDomicilio());
-            a.setFloat(7, usuario.getSaldo());
-            a.setInt(8, usuario.getId_usuario());
+            PreparedStatement statement = connection.prepareStatement(actualizar, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, usuario.getCorreo());
+            statement.setString(2, usuario.getContrasena());
+            statement.setString(3, usuario.getNombre_completo());
+            statement.setDate(4, (Date) usuario.getFecha_nacimiento());
+            statement.setInt(5, usuario.getEdad());
+            statement.setString(6, usuario.getDomicilio());
+            statement.setFloat(7, usuario.getSaldo());
+            statement.setInt(8, usuario.getId_usuario());
             
-            a.executeUpdate();
+            statement.executeUpdate();
             
             return true;
         } catch(SQLException e) {
@@ -84,12 +84,12 @@ public class UsuariosDAO implements IUsuariosDAO {
     @Override
     public Usuario consultarUsuario(int id_usuario) {
         try {
-            Connection c = conexion.crearConexion();
+            Connection connection = conexion.crearConexion();
             String buscarProducto = "SELECT * FROM usuarios WHERE id_usuario = ?";
-            PreparedStatement b = c.prepareStatement(buscarProducto);
-            b.setInt(1, id_usuario);
+            PreparedStatement statement = connection.prepareStatement(buscarProducto);
+            statement.setInt(1, id_usuario);
             
-            ResultSet resultado = b.executeQuery();
+            ResultSet resultado = statement.executeQuery();
             
             if(resultado.next()) {
                 Usuario u = new Usuario();
