e4bed12d38b82242a91e17eed23a3bab17191b90
YeBoiAaron <60552917+YeBoiAaron@users.noreply.github.com>
2024-09-20 20:02:01 -0700

    Actualizados stored procedure, triggers, y añadido event para reservas

--- ticketwizard.sql
+++ ticketwizard.sql
@@ -29,9 +29,8 @@ CREATE TABLE IF NOT EXISTS boletos (
     numero_serie VARCHAR(8) NOT NULL UNIQUE,
     fila VARCHAR(10),
     asiento VARCHAR(10),
-    identificador_asiento INT NOT NULL,
-    precio_original DECIMAL(10, 2) NOT NULL,
-    estado ENUM('vendido', 'disponible') DEFAULT 'disponible',
+    numero_control VARCHAR (16),
+    estado ENUM('vendido', 'disponible', 'reservado') DEFAULT 'vendido',
     es_reventa BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (evento_id) REFERENCES eventos(evento_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id)
@@ -57,13 +56,13 @@ CREATE TABLE IF NOT EXISTS administradores (
     correo_electronico VARCHAR(100) NOT NULL UNIQUE,
     contraseña VARCHAR(255) NOT NULL,
     nombre_completo VARCHAR(100) NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS reservas_boletos (
     reserva_id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT,
     boleto_id INT,
-    fecha_reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     estado ENUM('reservado', 'liberado') DEFAULT 'reservado',
     fecha_expiracion TIMESTAMP AS (DATE_ADD(fecha_reserva, INTERVAL 10 MINUTE)),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
@@ -74,26 +73,54 @@ DELIMITER //
 CREATE PROCEDURE transaccion(
     IN comprador_id INT, 
     IN vendedor_id INT, 
-    IN boleto_id INT,
+    IN id_boleto INT,
     IN monto DECIMAL(10, 2), 
     IN tipo_transaccion ENUM('compra', 'reventa')
 )
 BEGIN
     DECLARE comision DECIMAL(10, 2);
-    DECLARE saldo
-
+    DECLARE saldo_comprador DECIMAL(10, 2);
+    SELECT saldo 
+    INTO saldo_comprador 
+    FROM usuarios 
+    WHERE id_usuario = comprador_id;
+	
     IF tipo_transaccion = 'compra' THEN
         START TRANSACTION;
+		IF saldo >= monto THEN
+			UPDATE usuarios
+			SET saldo = saldo - monto
+			WHERE usuario_id = comprador_id;
 
-        UPDATE usuarios
-        SET saldo = saldo - monto
-        WHERE usuario_id = comprador_id;
+			SET comision = monto * 0.03;
+			UPDATE usuarios
+			SET saldo = saldo + (monto - comision)
+			WHERE usuario_id = vendedor_id;
+        
+			UPDATE boletos
+			SET usuario_id = comprador_id
+			WHERE boleto_id = id_boleto;
+        
+			UPDATE boletos
+			SET numero_serie = SUBSTRING(MD5(RAND()) FROM 1 FOR 8)
+			WHERE boleto_id = id_boleto;
+        
+			UPDATE boletos
+			SET estado = 'vendido'
+			WHERE boleto_id = id_boleto;
 
-        SET comision = monto * 0.03;
-        UPDATE usuarios
-        SET saldo = saldo + (monto - comision)
-        WHERE usuario_id = vendedor_id;
-
+			COMMIT;
+		ELSEIF saldo < monto THEN
+			INSERT INTO reservas_boletos (usuario_id, boleto_id) 
+            VALUES (comprador_id, id_boleto);
+        END IF;
+	ELSEIF tipo_transaccion = 'reventa' THEN
+		START TRANSACTION;
+        
+        UPDATE boletos
+        SET estado = 'disponible'
+        WHERE boleto_id = id_boleto;
+        
         COMMIT;
     END IF;
 END;
@@ -101,11 +128,47 @@ END;
 DELIMITER ;
 
 DELIMITER //
-CREATE TRIGGER actualizar_saldos_compra
-AFTER INSERT ON transacciones
+CREATE TRIGGER generar_numero_serie
+AFTER INSERT ON boletos
 FOR EACH ROW
 BEGIN
-    CALL actualizar_saldos_compra_procedure(NEW.comprador_id, NEW.vendedor_id, NEW.boleto_id, NEW.monto, NEW.tipo_transaccion);
+	UPDATE boletos
+	SET numero_serie = SUBSTRING(MD5(RAND()) FROM 1 FOR 8)
+	WHERE boleto_id = NEW.boleto_id;
 END;
 //
 DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER realizar_transaccion
+AFTER INSERT ON transacciones
+FOR EACH ROW
+BEGIN
+    CALL transaccion(NEW.comprador_id, NEW.vendedor_id, NEW.boleto_id, NEW.monto, NEW.tipo_transaccion);
+END;
+//
+DELIMITER ;
+
+DELIMITER //
+CREATE EVENT IF NOT EXISTS actualizar_reserva
+ON SCHEDULE EVERY 1 MINUTE
+DO
+BEGIN
+
+	UPDATE boletos b
+    JOIN reservas_boletos rb ON b.boleto_id = rb.boleto_id
+    SET b.estado = 'disponible'
+    WHERE b.estado = 'reservado'
+	AND TIMESTAMPDIFF(MINUTE, rb.fecha_reserva, NOW()) >= 10;
+    
+    UPDATE reservas_boletos rb
+    JOIN boletos b ON rb.boleto_id = b.boleto_id
+    SET rb.estado = 'liberado'
+    WHERE rb.estado = 'reservado'
+    AND TIMESTAMPDIFF(MINUTE, rb.fecha_reserva, NOW()) >= 10;
+END;
+//
+DELIMITER ;
+
+
+
